136a3eaeedb40a48938c764252a660abef265af3
Tony Cook <tony@develop-help.com>
2010-06-15 11:58:05 +0000

    0.009 release

--- Screenshot.pm
+++ Screenshot.pm
@@ -10,7 +10,7 @@ push @ISA, 'Exporter';
 BEGIN {
   require Exporter;
   @ISA = qw(Exporter);
-  $VERSION = '0.008';
+  $VERSION = '0.009';
   eval {
     # try XSLoader first, DynaLoader has annoying baggage
     require XSLoader;
