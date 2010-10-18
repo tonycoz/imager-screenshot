956a3577921722391829a70a3e80354c52cb25fe
Tony Cook <tony@develop-help.com>
2010-10-18 10:02:30 +0000

    handle string form of number passed as hwnd

--- svwin32.c
+++ svwin32.c
@@ -4,15 +4,15 @@ SSHWND
 hwnd_from_sv(pTHX_ SV *sv) {
   SvGETMAGIC(sv);
 
+  if (!SvOK(sv)) {
+    return 0;
+  }
   if (SvPOK(sv)) {
     STRLEN len;
     char const *p = SvPV_nomg(sv, len);
 
     if (len == 6 && strEQ(p, "active")) {
       return (SSHWND)GetForegroundWindow();
-    }
-    else {
-      return (SSHWND)NULL;
     }
   }
 
